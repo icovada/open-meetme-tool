fa8784732eb6914d2cc13c31c2940243eecdfa78
icovada <ftabbo@gmail.com>
2022-11-09 17:17:47 +0100

    Enable Django Admin

--- meetme/meetme_app/admin.py
+++ meetme/meetme_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Event, UserProfile, MeetingRequest, TimeSlot
+
+# customize default django admin site html meta
+# default: "Django Administration"
+admin.site.site_header = "Open Meet-Me Tool"
+
+admin.site.register(Event)
+admin.site.register(UserProfile)
+admin.site.register(MeetingRequest)
+admin.site.register(TimeSlot)

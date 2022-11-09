e021336d5178bff89ecfb305c68117114e4d4a96
icovada <ftabbo@gmail.com>
2022-11-09 17:19:30 +0100

    Activate signals

--- meetme/meetme_app/apps.py
+++ meetme/meetme_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MeetmeAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meetme_app'
+
+    def ready(self):
+        import meetme_app.signals

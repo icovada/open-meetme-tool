89d8d87dbfbaed7a216a607f4d1a27505b99d99c
icovada <ftabbo@gmail.com>
2022-11-09 21:12:12 +0100

    Init pytest

--- meetme/meetme_app/tests.py
+++ meetme/meetme_app/tests.py
@@ -1,3 +1,13 @@
+import os
 from django.test import TestCase
+from django import setup
 
+from .models import Event, MeetingRequest, TimeSlot
 # Create your tests here.
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+setup()
+
+class TestAntani(TestCase):
+    def test_antani_discover(self):
+        Event(name="ciao")

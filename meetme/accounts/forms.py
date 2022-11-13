e05a35540453ceef31bc71226f390e2b1f6cd12b
icovada <ftabbo@gmail.com>
2022-11-13 21:32:55 +0100

    Override login form

--- meetme/accounts/forms.py
+++ meetme/accounts/forms.py
@@ -1,6 +1,7 @@
-from allauth.account.forms import SignupForm
+from allauth.account.forms import SignupForm, LoginForm
 from django import forms
-from .models import CustomUser
+
+BOOTSTRAP5_TEXT_CLASS = 'form-control'
 
 class SimpleSignupForm(SignupForm):
     first_name = forms.CharField(max_length=150)
@@ -13,3 +14,14 @@ class SimpleSignupForm(SignupForm):
         user.username = self.cleaned_data['email']
         user.save()
         return user
+
+
+class BSLoginForm(LoginForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['login'].widget = forms.TextInput(
+            attrs={'type': 'email', 'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['password'].widget = forms.PasswordInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['remember'].widget = forms.CheckboxInput(
+            attrs={'class': 'form-check-input'})
--- meetme/meetme/settings.py
+++ meetme/meetme/settings.py
@@ -161,7 +161,10 @@ LOGIN_URL = "/accounts/login"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 AUTH_USER_MODEL = 'accounts.CustomUser'
-ACCOUNT_FORMS = {'signup': 'accounts.forms.SimpleSignupForm'}
+ACCOUNT_FORMS = {
+    'signup': 'accounts.forms.SimpleSignupForm',
+    'login': 'accounts.forms.BSLoginForm',
+}
 
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True

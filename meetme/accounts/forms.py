c3e4582e6ba75481cb452df8d10a2e3037c0afef
icovada <ftabbo@gmail.com>
2022-11-13 19:37:20 +0100

    Change signup form

---
+++ meetme/accounts/forms.py
@@ -0,0 +1,14 @@
+from allauth.account.forms import SignupForm
+from django import forms
+from .models import CustomUser
+
+class SimpleSignupForm(SignupForm):
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150)
+    company = forms.CharField(max_length=30, label='Company')
+
+    def save(self, request):
+        user = super(SimpleSignupForm, self).save(request)
+        user.company = self.cleaned_data['company']
+        user.save()
+        return user
--- meetme/meetme/settings.py
+++ meetme/meetme/settings.py
@@ -161,3 +161,4 @@ LOGIN_URL = "/accounts/login"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 AUTH_USER_MODEL = 'accounts.CustomUser'
+ACCOUNT_FORMS = {'signup': 'accounts.forms.SimpleSignupForm'}

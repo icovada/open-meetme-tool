1f657b66423f8f5bcea755792f31030e09d580d4
icovada <ftabbo@gmail.com>
2022-11-13 21:49:58 +0100

    Style it up

--- meetme/accounts/forms.py
+++ meetme/accounts/forms.py
@@ -1,15 +1,31 @@
-from allauth.account.forms import SignupForm, LoginForm
+from allauth.account.forms import LoginForm, SignupForm
 from django import forms
 
 BOOTSTRAP5_TEXT_CLASS = 'form-control'
 
-class SimpleSignupForm(SignupForm):
+class BSSignupForm(SignupForm):
     first_name = forms.CharField(max_length=150)
     last_name = forms.CharField(max_length=150)
     company = forms.CharField(max_length=30, label='Company')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget = forms.EmailInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['first_name'].widget = forms.TextInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['last_name'].widget = forms.TextInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['company'].widget = forms.TextInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['password1'].widget = forms.PasswordInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['password2'].widget = forms.PasswordInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
+
+
     def save(self, request):
-        user = super(SimpleSignupForm, self).save(request)
+        user = super().save(request)
         user.company = self.cleaned_data['company']
         user.username = self.cleaned_data['email']
         user.save()
@@ -19,8 +35,8 @@ class SimpleSignupForm(SignupForm):
 class BSLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['login'].widget = forms.TextInput(
-            attrs={'type': 'email', 'class': BOOTSTRAP5_TEXT_CLASS})
+        self.fields['login'].widget = forms.EmailInput(
+            attrs={'class': BOOTSTRAP5_TEXT_CLASS})
         self.fields['password'].widget = forms.PasswordInput(
             attrs={'class': BOOTSTRAP5_TEXT_CLASS})
         self.fields['remember'].widget = forms.CheckboxInput(
--- meetme/meetme/settings.py
+++ meetme/meetme/settings.py
@@ -162,7 +162,7 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 AUTH_USER_MODEL = 'accounts.CustomUser'
 ACCOUNT_FORMS = {
-    'signup': 'accounts.forms.SimpleSignupForm',
+    'signup': 'accounts.forms.BSSignupForm',
     'login': 'accounts.forms.BSLoginForm',
 }
 
